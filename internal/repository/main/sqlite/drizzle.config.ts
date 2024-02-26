c81550344c6fd1e71a56396877819d4744f9035c
upikoth <upikoth@gmail.com>
2024-02-26 22:00:50 +0300

    fix: поправил билд

--- internal/repository/main/sqlite/drizzle.config.ts
+++ internal/repository/main/sqlite/drizzle.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from 'drizzle-kit'
 
 export default {
-	schema: './internal/repository/sqlite/schema.ts',
-	out: './internal/repository/sqlite/migrations',
+	schema: './internal/repository/main/sqlite/schema.ts',
+	out: './internal/repository/main/sqlite/migrations',
 	driver: 'libsql',
 	dbCredentials: { url: 'database.sqlite' }
 } satisfies Config
