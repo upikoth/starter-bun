91a66bd6da1d61ea28e4a119d63d4b9b4ee562c7
upikoth <upikoth@gmail.com>
2024-03-12 00:10:25 +0300

    chore: рефакторинг именования и структуры openapi и примеров

--- __tests__/e2e/api-v1-health.spec.ts
+++ __tests__/e2e/api-v1-health.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from 'bun:test'
 
 import { getActiveServer } from '../index'
-import successResponse from '../../docs/examples/responses/common/success.json'
+import defaultSuccess from '../../docs/examples/default/200.json'
 
 test('/api/v1/health', async () => {
 	const server = getActiveServer()
@@ -9,5 +9,5 @@ test('/api/v1/health', async () => {
 	const response = await server.fetch(`${server.url.origin}/api/v1/health`)
 	const responseJson = await response.json()
 
-	expect(responseJson).toMatchObject(successResponse)
+	expect(responseJson).toMatchObject(defaultSuccess)
 })
--- __tests__/e2e/not-found.spec.ts
+++ __tests__/e2e/not-found.spec.ts
@@ -1,13 +1,13 @@
 import { expect, test } from 'bun:test'
 
 import { getActiveServer } from '../index'
-import unknownMethodResponse from '../../docs/examples/responses/common/unknown-method.json'
+import defaultNotFound from '../../docs/examples/default/404.json'
 
 test('Проверка ответа на неизвестный запрос', async () => {
 	const server = getActiveServer()
 
-	const response = await server.fetch(`${server.url.origin}/api/v1/unknown-method`)
+	const response = await server.fetch(`${server.url.origin}/api/v1/404`)
 	const responseJson = await response.json()
 
-	expect(responseJson).toMatchObject(unknownMethodResponse)
+	expect(responseJson).toMatchObject(defaultNotFound)
 })
