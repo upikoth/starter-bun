647b23361c5cdc2b139c7247f99309e3d857cc90
upikoth <upikoth@gmail.com>
2024-03-12 20:33:30 +0300

    chore: добавил возможность передать авторизационный токен в swagger

--- internal/utils/__tests__/session.spec.ts
+++ internal/utils/__tests__/session.spec.ts
@@ -4,13 +4,27 @@ import { AUTHORIZATION_HEADER } from '@/constants'
 
 import { getSessionFromRequest } from '@/utils'
 
-test('Проверяет что getSessionFromRequest возвращает значение сессии', () => {
+test('Проверяет что getSessionFromRequest возвращает значение сессии, если передать ее в cookie', () => {
 	const sessionValue = 'sessionValueDummy'
 
 	const req = new Request({
 		url: 'http://localhost:8080/dummy-url',
 		headers: {
 			Cookie: `${AUTHORIZATION_HEADER}=${sessionValue}`
+		}
+	})
+
+	expect(getSessionFromRequest(req)).toBe(sessionValue)
+})
+
+test(`Проверяет что getSessionFromRequest возвращает значение сессии,
+			если передать ее в header SwaggerAuthorization`, () => {
+	const sessionValue = 'sessionValueDummy'
+
+	const req = new Request({
+		url: 'http://localhost:8080/dummy-url',
+		headers: {
+			SwaggerAuthorization: sessionValue
 		}
 	})
 
--- internal/utils/session.ts
+++ internal/utils/session.ts
@@ -4,7 +4,7 @@ import { AUTHORIZATION_HEADER } from '@/constants'
 
 export function getSessionFromRequest(req: Request) {
 	const parsedCookie = cookie.parse(req.headers.get('Cookie') || '')
-	const session = parsedCookie[AUTHORIZATION_HEADER] || ''
+	const session = parsedCookie[AUTHORIZATION_HEADER] || req.headers.get('SwaggerAuthorization') || ''
 
 	return session
 }
