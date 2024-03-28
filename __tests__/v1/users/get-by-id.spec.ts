e0b17343b836bf3e485cf73557cbf01006a18121
upikoth <upikoth@gmail.com>
2024-03-29 00:19:29 +0300

    chore: добавлены тесты для метода получения информации о пользователе

---
+++ __tests__/v1/users/get-by-id.spec.ts
@@ -0,0 +1,197 @@
+import {
+	expect, test, describe, mock
+} from 'bun:test'
+import examples from '@docs/examples'
+
+import { AUTHORIZATION_HEADER, ErorrStatusEnum, HttpMethod } from '@/constants'
+
+import mainExamples from '@/repository/main/docs/examples'
+
+import { mainRequestHandler } from '@/app'
+
+describe('/api/v1/users/:id - Get', () => {
+	test('Если не передать сессионный токен, метод вернет ответ что пользователь не авторизован', async () => {
+		const request = new Request({
+			url: 'https://example-host.ru/api/v1/users/1',
+			method: HttpMethod.Get
+		})
+
+		const response = await mainRequestHandler(request)
+		const responseJson = await response.json()
+
+		expect(responseJson).toMatchObject(examples.default.responses.notAuthorized)
+	})
+
+	test(`Если передать токен пользователя с ролью User, метод вернет успешный ответ,
+				если пользователь запрашивает информацию о себе`, async () => {
+		mock.module('@/repository', () => ({
+			default: {
+				main: {
+					sessions: {
+						getBySession: () => mainExamples.sessions.responses.getBySession.success
+					},
+					users: {
+						getById: () => mainExamples.users.responses.getById.userActiveSuccess
+					}
+				}
+			}
+		}))
+
+		const currentUserId = mainExamples.users.responses.getById.userActiveSuccess.id
+
+		const request = new Request({
+			url: `https://example-host.ru/api/v1/users/${currentUserId}`,
+			method: HttpMethod.Get,
+			headers: {
+				Cookie: `${AUTHORIZATION_HEADER}=session-test-example`
+			}
+		})
+
+		const response = await mainRequestHandler(request)
+		const responseJson = await response.json()
+
+		expect(responseJson).toMatchObject(examples.users.responses.getById.userActiveSuccess)
+	})
+
+	test(`Если передать токен пользователя с ролью User, метод вернет ошибку
+				если пользователь запрашивает информацию о других пользователях`, async () => {
+		mock.module('@/repository', () => ({
+			default: {
+				main: {
+					sessions: {
+						getBySession: () => mainExamples.sessions.responses.getBySession.success
+					},
+					users: {
+						getById: () => mainExamples.users.responses.getById.userActiveSuccess
+					}
+				}
+			}
+		}))
+
+		const anotherUserId = mainExamples.users.responses.getById.userActiveSuccess.id + 1
+
+		const request = new Request({
+			url: `https://example-host.ru/api/v1/users/${anotherUserId}`,
+			method: HttpMethod.Get,
+			headers: {
+				Cookie: `${AUTHORIZATION_HEADER}=session-test-example`
+			}
+		})
+
+		const response = await mainRequestHandler(request)
+		const responseJson = await response.json()
+
+		expect(responseJson).toMatchObject(examples.default.responses.forbidden)
+	})
+
+	test('Если передать некорректно id, вернется ошибка', async () => {
+		mock.module('@/repository', () => ({
+			default: {
+				main: {
+					sessions: {
+						getBySession: () => mainExamples.sessions.responses.getBySession.success
+					},
+					users: {
+						getById: () => mainExamples.users.responses.getById.adminActiveSuccess
+					}
+				}
+			}
+		}))
+
+		const request = new Request({
+			url: 'https://example-host.ru/api/v1/users/wrong-id-example',
+			method: HttpMethod.Get,
+			headers: {
+				Cookie: `${AUTHORIZATION_HEADER}=session-test-example`
+			}
+		})
+
+		const response = await mainRequestHandler(request)
+
+		expect(response.status).toBe(ErorrStatusEnum.BadRequest)
+	})
+
+	test('Если передать токен пользователя с ролью Admin, метод вернет информацию о любом пользователе', async () => {
+		mock.module('@/repository', () => ({
+			default: {
+				main: {
+					sessions: {
+						getBySession: () => mainExamples.sessions.responses.getBySession.success
+					},
+					users: {
+						getById: () => mainExamples.users.responses.getById.adminActiveSuccess
+					}
+				}
+			}
+		}))
+
+		const request = new Request({
+			url: 'https://example-host.ru/api/v1/users/10',
+			method: HttpMethod.Get,
+			headers: {
+				Cookie: `${AUTHORIZATION_HEADER}=session-test-example`
+			}
+		})
+
+		const response = await mainRequestHandler(request)
+		const responseJson = await response.json()
+
+		expect(responseJson).toMatchObject(examples.users.responses.getById.adminActiveSuccess)
+	})
+
+	test(`Если передать токен пользователя с ролью Admin и запросить информацию о несуществующем пользователе,
+				метод вернет ошибку`, async () => {
+		mock.module('@/repository', () => ({
+			default: {
+				main: {
+					sessions: {
+						getBySession: () => mainExamples.sessions.responses.getBySession.success
+					},
+					users: {
+						getById: () => undefined
+					}
+				}
+			}
+		}))
+
+		const request = new Request({
+			url: 'https://example-host.ru/api/v1/users/10',
+			method: HttpMethod.Get,
+			headers: {
+				Cookie: `${AUTHORIZATION_HEADER}=session-test-example`
+			}
+		})
+
+		const response = await mainRequestHandler(request)
+
+		expect(response.status).toBe(ErorrStatusEnum.BadRequest)
+	})
+
+	test('Если передать токен пользователя с ролью SuperAdmin, метод вернет список пользователей', async () => {
+		mock.module('@/repository', () => ({
+			default: {
+				main: {
+					sessions: {
+						getBySession: () => mainExamples.sessions.responses.getBySession.success
+					},
+					users: {
+						getById: () => mainExamples.users.responses.getById.superAdminActiveSuccess
+					}
+				}
+			}
+		}))
+
+		const request = new Request({
+			url: 'https://example-host.ru/api/v1/users/10',
+			method: HttpMethod.Get,
+			headers: {
+				Cookie: `${AUTHORIZATION_HEADER}=session-test-example`
+			}
+		})
+
+		const response = await mainRequestHandler(request)
+		const responseJson = await response.json()
+
+		expect(responseJson).toMatchObject(examples.users.responses.getById.superAdminActiveSuccess)
+	})
+})
--- docs/examples/users/index.ts
+++ docs/examples/users/index.ts
@@ -1,9 +1,17 @@
 import getAllUsersSuccessResponse from './get-all/response-success.json'
 import getAllSUsersWrongRequest from './get-all/request-wrong.json'
+import getByIdActiveUserSuccessResponse from './get-by-id/response-success-user-active.json'
+import getByIdActiveAdminSuccessResponse from './get-by-id/response-success-admin-active.json'
+import getByIdActiveSuperAdminSuccessResponse from './get-by-id/response-success-super-admin-active.json'
 
 const responses = {
 	getAll: {
 		success: getAllUsersSuccessResponse
+	},
+	getById: {
+		userActiveSuccess: getByIdActiveUserSuccessResponse,
+		adminActiveSuccess: getByIdActiveAdminSuccessResponse,
+		superAdminActiveSuccess: getByIdActiveSuperAdminSuccessResponse
 	}
 }
 
